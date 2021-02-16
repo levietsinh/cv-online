0c52353fdad7f18a4e0847a794351c90e3c26554
levietsinh <levietsinh201194@gmail.com>
2021-02-16 12:36:55 +0700

    style header

--- js/home.js
+++ js/home.js
@@ -2,7 +2,15 @@ var resumeNavItem = document.querySelectorAll('.resume-item');
 var resumeContentItem = document.querySelectorAll('.resume-body');
 var content2 = document.querySelector('.content-2');
 var content3 = document.querySelector('.content-3');
+var header = document.querySelector('.header');
 
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 0) {
+    header.classList.add('style-scroll');
+  } else {
+    header.classList.remove('style-scroll');
+  }
+})
 function selectItem(index) {
   for (let i = 0; i < resumeNavItem.length; i++) {
     if (index === i) {
