bae597e1267795e80aacb3faf2da94291f841b9e
levietsinh <levietsinh201194@gmail.com>
2021-02-23 22:26:03 +0700

    fix contact and menu mobile

--- js/header.js
+++ js/header.js
@@ -2,6 +2,21 @@ var header = document.querySelector('.header');
 var headerMobile = document.querySelector('.header-mobile');
 var bodyEl = document.querySelector('body');
 
+var content2 = document.querySelector('.content-2');
+var content3 = document.querySelector('.content-3');
+
+var topOfResume = content2.offsetTop - 89;
+var topOfContact = content3.offsetTop - 89;
+var urlHash = location.hash;
+var navItem = document.querySelectorAll('.header__nav ul li');
+
+function isMobile() {
+  if (window.innerWidth > 767) return false;
+  else return true;
+}
+
+
+
 window.addEventListener('scroll', function () {
   if (window.scrollY > 0) {
     header.classList.add('style-scroll');
@@ -17,15 +32,93 @@ window.addEventListener('scroll', function () {
 
 function openMobileMenu() {
   headerMobile.style.display = "none";
-  header.style.display = "block";
+  header.classList.add('show');
+  header.classList.remove('hide');
   bodyEl.style.height = "100vh";
   bodyEl.style.overflow = "hidden";
 
 }
 function closeMobileMenu() {
   headerMobile.style.display = "block";
-  header.style.display = "none";
+  header.classList.remove('show');
+  header.classList.add('hide');
   bodyEl.style.height = "";
   bodyEl.style.overflow = "";
 
+}
+
+function toHome() {
+  if (isMobile()) closeMobileMenu();
+  window.scroll({ top: 0, behavior: "smooth" });
+  for (let index = 0; index < navItem.length; index++) {
+    if (index === 0) {
+      navItem[index].classList.add('selected');
+    } else {
+      navItem[index].classList.remove('selected');
+    }
+  }
+}
+
+function scrollToResume() {
+  if (isMobile()) {
+    closeMobileMenu();
+    window.scroll({ top: topOfResume + 30, behavior: "smooth" });
+  } else {
+    window.scroll({ top: topOfResume, behavior: "smooth" });
+
+  }
+  
+for (let index = 0; index < navItem.length; index++) {
+  if (index === 2) {
+    navItem[index].classList.add('selected');
+  } else {
+    navItem[index].classList.remove('selected');
+  }
+}
+}
+function scrollToContact() {
+  if (isMobile()) {
+    closeMobileMenu();
+    window.scroll({ top: topOfContact + 30, behavior: "smooth" });
+  } else {
+    window.scroll({ top: topOfContact, behavior: "smooth" });
+}
+for (let index = 0; index < navItem.length; index++) {
+  if (index === 3) {
+    navItem[index].classList.add('selected');
+  } else {
+    navItem[index].classList.remove('selected');
+  }
+}
+
+}
+
+
+if (urlHash === '#resume') {
+scrollToResume();
+for (let index = 0; index < navItem.length; index++) {
+  if (index === 2) {
+    navItem[index].classList.add('selected');
+  } else {
+    navItem[index].classList.remove('selected');
+  }
+}
+} 
+else if (urlHash === '#contact') {
+scrollToContact();
+for (let index = 0; index < navItem.length; index++) {
+  if (index === 3) {
+    navItem[index].classList.add('selected');
+  } else {
+    navItem[index].classList.remove('selected');
+  }
+}
+} else {
+for (let index = 0; index < navItem.length; index++) {
+  if (index === 0) {
+    navItem[index].classList.add('selected');
+  } else {
+    navItem[index].classList.remove('selected');
+  }
+}
 }
--- js/home.js
+++ js/home.js
@@ -1,13 +1,8 @@
+
+
+
 var resumeNavItem = document.querySelectorAll('.resume-item');
 var resumeContentItem = document.querySelectorAll('.resume-body');
-var content2 = document.querySelector('.content-2');
-var content3 = document.querySelector('.content-3');
-
-var topOfResume = content2.offsetTop - 89;
-var topOfContact = content3.offsetTop - 89;
-var urlHash = location.hash;
-var navItem = document.querySelectorAll('.header__nav ul li');
-
 
 function selectItem(index) {
   for (let i = 0; i < resumeNavItem.length; i++) {
@@ -21,65 +16,4 @@ function selectItem(index) {
     }  
   }
 }
-function toHome() {
-    window.scroll({ top: 0, behavior: "smooth" });
-    for (let index = 0; index < navItem.length; index++) {
-      if (index === 0) {
-        navItem[index].classList.add('selected');
-      } else {
-        navItem[index].classList.remove('selected');
-      }
-    }
-}
 
-function scrollToResume() {
-  window.scroll({ top: topOfResume, behavior: "smooth" });
-  for (let index = 0; index < navItem.length; index++) {
-    if (index === 2) {
-      navItem[index].classList.add('selected');
-    } else {
-      navItem[index].classList.remove('selected');
-    }
-  }
-}
-function scrollToContact() {
-  window.scroll({ top: topOfContact, behavior: "smooth" });
-  for (let index = 0; index < navItem.length; index++) {
-    if (index === 3) {
-      navItem[index].classList.add('selected');
-    } else {
-      navItem[index].classList.remove('selected');
-    }
-  }
-
-}
-
-
-if (urlHash === '#resume') {
-  scrollToResume();
-  for (let index = 0; index < navItem.length; index++) {
-    if (index === 2) {
-      navItem[index].classList.add('selected');
-    } else {
-      navItem[index].classList.remove('selected');
-    }
-  }
-} 
-else if (urlHash === '#contact') {
-  scrollToContact();
-  for (let index = 0; index < navItem.length; index++) {
-    if (index === 3) {
-      navItem[index].classList.add('selected');
-    } else {
-      navItem[index].classList.remove('selected');
-    }
-  }
-} else {
-  for (let index = 0; index < navItem.length; index++) {
-    if (index === 0) {
-      navItem[index].classList.add('selected');
-    } else {
-      navItem[index].classList.remove('selected');
-    }
-  }
-}
