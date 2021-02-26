173c548767c3fbbf574752b1481034557644cbf9
levietsinh <levietsinh201194@gmail.com>
2021-02-26 21:51:38 +0700

    style resume content

--- js/home.js
+++ js/home.js
@@ -3,8 +3,17 @@
 
 var resumeNavItem = document.querySelectorAll('.resume-item');
 var resumeContentItem = document.querySelectorAll('.resume-body');
+var resumeRight = document.querySelector('.resume-right');
+
+function isMobile() {
+  if (window.innerWidth > 767) return false;
+  else return true;
+}
 
 function selectItem(index) {
+  if (isMobile()) {
+    showResumeContent();
+  }
   for (let i = 0; i < resumeNavItem.length; i++) {
     if (index === i) {
       resumeNavItem[i].classList.add('active');
@@ -17,3 +26,11 @@ function selectItem(index) {
   }
 }
 
+function showResumeContent() {
+  resumeRight.classList.add('show');
+  resumeRight.classList.remove('hide');
+}
+function showResumeMenu() {
+  resumeRight.classList.remove('show');
+  resumeRight.classList.add('hide');
+}
